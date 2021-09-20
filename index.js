ec5b2da0293a9679ccef88e7d2615e7ccb44fbbb
tom <tom@tom>
2021-09-20 17:08:37 +0200

    add comments

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ function verPlaatsOptionNaar(option, id) {
 document.getElementById("allesNaarRechts").onclick = function() {
     const options = document.getElementById("links").options;
     /*
+
+    //werkt:
     [...options].forEach(
         option => {verPlaatsOptionNaar(option, "rechts")}
     )
     
-
+    //werkt niet:
     for (const option of options) {
         verPlaatsOptionNaar(option, "rechts");
     }
-
+    
+    //werkt:
     for (const option of [...options]) {
         verPlaatsOptionNaar(option, "rechts");
     }
 
     */
-
+    
+    //werkt nietk
     for (let i = 0; i < options.length; i++){
         verPlaatsOptionNaar(options[i], "rechts");
     }
